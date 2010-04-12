d759ba01a43009845fbf8535ae6c7d7665df4911
James Pozdena <jpoz@jpoz.net>
2010-04-12 13:17:03 -0700

    adding in feedback service

--- lib/apns/core.rb
+++ lib/apns/core.rb
@@ -32,6 +32,23 @@ module APNS
     sock.close
   end
   
+  def self.feedback
+    sock, ssl = self.feedback_connection
+    
+    apns_feedback = []
+    
+    while line = sock.gets   # Read lines from the socket
+      line.strip!
+      f = line.unpack('N1n1H140')
+      apns_feedback << [Time.at(f[0]), f[2]]
+    end
+    
+    ssl.close
+    sock.close
+    
+    return apns_feedback
+  end
+  
   protected
 
   def self.packaged_notification(device_token, message)
@@ -70,6 +87,24 @@ module APNS
     context.key  = OpenSSL::PKey::RSA.new(File.read(self.pem), self.pass)
 
     sock         = TCPSocket.new(self.host, self.port)
+    ssl          = OpenSSL::SSL::SSLSocket.new(sock,context)
+    ssl.connect
+
+    return sock, ssl
+  end
+  
+  def self.feedback_connection
+    raise "The path to your pem file is not set. (APNS.pem = /path/to/cert.pem)" unless self.pem
+    raise "The path to your pem file does not exist!" unless File.exist?(self.pem)
+    
+    context      = OpenSSL::SSL::SSLContext.new
+    context.cert = OpenSSL::X509::Certificate.new(File.read(self.pem))
+    context.key  = OpenSSL::PKey::RSA.new(File.read(self.pem), self.pass)
+
+    fhost = self.host.gsub!('gateway','feedback')
+    puts fhost
+    
+    sock         = TCPSocket.new(fhost, 2196)
     ssl          = OpenSSL::SSL::SSLSocket.new(sock,context)
     ssl.connect
 
