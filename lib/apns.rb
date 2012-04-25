9891c55f63bb98c3ffb4d9f297f10e6e5cfa0e7b
John Crepezzi <john.crepezzi@gmail.com>
2012-04-25 13:34:56 -0400

    Move core into apns.rb

--- lib/apns.rb
+++ lib/apns.rb
@@ -1,4 +1,14 @@
 require 'apns/connector'
-require 'apns/client'
-require 'apns/core'
 require 'apns/notification'
+
+module APNS
+
+  # We are a connector
+  include Connector
+
+  # And we have a class Client, which is also a connector
+  class Client
+    include Connector
+  end
+
+end
--- lib/apns/core.rb
+++
@@ -1,5 +0,0 @@
-module APNS
-
-  include APNS::Connector
-
-end
