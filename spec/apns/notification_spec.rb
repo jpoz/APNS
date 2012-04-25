9119f30fe9b71f23d680251af7da5ca18d6c7f1f
John Crepezzi <john.crepezzi@gmail.com>
2012-04-25 13:13:22 -0400

    Whitespace cleanup

--- spec/apns/notification_spec.rb
+++ spec/apns/notification_spec.rb
@@ -1,32 +1,32 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe APNS::Notification do
-  
+
   it "should take a string as the message" do
     n = APNS::Notification.new('device_token', 'Hello')
     n.alert.should == 'Hello'
   end
-  
+
   it "should take a hash as the message" do
     n = APNS::Notification.new('device_token', {:alert => 'Hello iPhone', :badge => 3})
     n.alert.should == "Hello iPhone"
     n.badge.should == 3
   end
-  
+
   describe '#packaged_message' do
-    
+
     it "should return JSON with notification information" do
       n = APNS::Notification.new('device_token', {:alert => 'Hello iPhone', :badge => 3, :sound => 'awesome.caf'})
       n.packaged_message.should  == "{\"aps\":{\"badge\":3,\"sound\":\"awesome.caf\",\"alert\":\"Hello iPhone\"}}"
     end
-    
+
     it "should not include keys that are empty in the JSON" do
       n = APNS::Notification.new('device_token', {:badge => 3})
       n.packaged_message.should == "{\"aps\":{\"badge\":3}}"
     end
-    
+
   end
-  
+
   describe '#package_token' do
     it "should package the token" do
       n = APNS::Notification.new('<5b51030d d5bad758 fbad5004 bad35c31 e4e0f550 f77f20d4 f737bf8d 3d5524c6>', 'a')
@@ -40,5 +40,5 @@ describe APNS::Notification do
       Base64.encode64(n.packaged_notification).should == "AAAg3vLO/YTnAEB7ImFwcyI6eyJiYWRnZSI6Mywic291bmQiOiJhd2Vzb21l\nLmNhZiIsImFsZXJ0IjoiSGVsbG8gaVBob25lIn19\n"
     end
   end
-  
+
 end
